be7c0c84e4eb1ed45a40fb252f90607072e9aa14
Abir-ux <abir573912@gmail.com>
2020-07-06 18:11:40 +0600

    finished ema-jhon

---
+++ src/components/Notfound/Notfound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Notfound.css';
+
+const Notfound = () => {
+    return (
+        <div>
+            <h1 className="h1h1">Sorry, <span className="h1">page not found!!!!!</span></h1>
+            <h2 className="h2h2">404</h2>
+            
+        </div>
+    );
+};
+
+export default Notfound;
